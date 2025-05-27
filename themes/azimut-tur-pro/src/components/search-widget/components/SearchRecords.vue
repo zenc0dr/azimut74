8dd0704d819c931d75f599db112c2b76499c162d
Alex Blaze <zen@8ber.ru>
2025-05-27 15:05:37 +0400

    Some fixes

--- themes/azimut-tur-pro/src/components/search-widget/components/SearchRecords.vue
+++ themes/azimut-tur-pro/src/components/search-widget/components/SearchRecords.vue
@@ -187,6 +187,13 @@ export default {
     methods: {
         getDays(item) {
             console.log('Обработка записи', item)
+
+            if (item.date && item.date.d1 !== undefined) {
+                console.log('Свойство d1 существует');
+            } else {
+                console.log('Свойство d1 не существует', item);
+            }
+
             return `${ item.date.d1 }, ${ item.date.d1d } (${ item.date.t1 }) — ${ item.date.d2 }, ${ item.date.d2d } (${ item.date.t2 })`
         },
         hasDiscounts(record) {
