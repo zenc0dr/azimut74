b8f65998e614f675d7dcfae62119d35e3ef975a4
Alex Blaze <zen@8ber.ru>
2025-07-13 18:24:09 +0400

    Some fixes

--- plugins/zen/master/console/CheckSystem.php
+++ plugins/zen/master/console/CheckSystem.php
@@ -15,8 +15,19 @@ class CheckSystem extends Command
         $min = 1000;
         $size_mb = $this->getFreeSize();
         if ($size_mb < $min) {
-            $text = "Внимание! На диске осталось менее $min mb";
-            master()->telegram()->sendMessage($text);
+            if ($size_mb < 300) {
+                master()->telegram()->sendMessage(
+                    "Внимание! На диске осталось менее 300 mb, приступаю к очистке"
+                );
+                $this->clearCache();
+                master()->telegram()->sendMessage(
+                    "Кэш очищен, место освобождено"
+                );
+            } else {
+                master()->telegram()->sendMessage(
+                    "Внимание! На диске осталось менее $min mb"
+                );
+            }
         }
     }
 
@@ -30,5 +41,10 @@ class CheckSystem extends Command
         $size_mb = trim($size_mb);
         $size_mb = intval($size_mb);
         return $size_mb;
+    }
+
+    private function clearCache(): void
+    {
+        shell_exec("php /app/artisan cache:clear");
     }
 }
