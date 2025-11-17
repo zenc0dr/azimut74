ef53090e6b94d21f89e321c948af40de9de85550
Al Blaze <zen@8ber.ru>
2025-11-17 17:42:54 +0400

    Добавление виджета

--- plugins/zen/worker/Plugin.php
+++ plugins/zen/worker/Plugin.php
@@ -24,6 +24,7 @@ class Plugin extends PluginBase
         $this->registerConsoleCommand('worker:volga-parse', 'Zen\Worker\Console\volga\VolgaParse');
         $this->registerConsoleCommand('worker:germes-parse', 'Zen\Worker\Console\germes\GermesParse');
         $this->registerConsoleCommand('worker:waterway-parse', 'Zen\Worker\Console\waterway\WaterwayParse');
+        $this->registerConsoleCommand('worker:clear-cache', 'Zen\Worker\Console\ClearCache');
     }
 
     public function boot()
@@ -37,6 +38,10 @@ class Plugin extends PluginBase
         return [
             'Zen\Worker\ReportWidgets\ClearCruisesWidget' => [
                 'label'   => 'Очистка базы круизов',
+                'context' => 'dashboard'
+            ],
+            'Zen\Worker\ReportWidgets\CacheStatsWidget' => [
+                'label'   => 'Статистика кеша парсеров',
                 'context' => 'dashboard'
             ]
         ];
