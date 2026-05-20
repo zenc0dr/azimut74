1a13e633518089bbd4573eaafa95157eb6adcda3
Al Blaze <zen@8ber.ru>
2026-05-20 15:37:50 +0400

    https://azimuth-tour.kaiten.ru/65025752

--- plugins/mcmraak/rivercrs/classes/exist/Gama.php
+++ plugins/mcmraak/rivercrs/classes/exist/Gama.php
@@ -30,13 +30,32 @@ class Gama extends Exist
         }
 
         foreach ($cabins as $cabin) {
-            $gama_cabin_id = $cabin['@attributes']['id'];
+            $gama_cabin_id = intval($cabin['@attributes']['id']);
             $gama_cabin_num = $cabin['@attributes']['name'];
-            $places = intval($cabin['@attributes']['places']);
 
             $costs = $cabin['Cost'] ?? [];
             if (isset($costs['@attributes'])) {
                 $costs = [$costs];
+            }
+
+            $category_data = $gama->getGamaCategory($gama_cabin_id, $gama_ship_id);
+            if (!$category_data) {
+                continue;
+            }
+
+            // Как в GamaV2::getCruisePrices — ищем категорию по полному имени (например "6|13"),
+            // а не по усечённому gama_name в БД (например "6").
+            $category_name = $category_data['name'];
+            $places = intval($category_data['places']);
+            $cabin_category_id = $gama->getCabinCategoryId(
+                $category_name,
+                $checkin->motorship_id,
+                'gama',
+                $places > 0 ? $places : null
+            );
+
+            if (!$cabin_category_id) {
+                continue;
             }
 
             foreach ($costs as $cost) {
@@ -48,22 +67,23 @@ class Gama extends Exist
                     continue;
                 }
 
-                $category_data = $gama->getGamaCategory($gama_cabin_id, $gama_ship_id);
-                if (!$category_data) {
+                $price_value = intval($cost['std_3'] ?? 0);
+                if ($price_value <= 0) {
                     continue;
                 }
 
                 $record = $this->addRecord([
                     'deck_name' => $category_data['deck_name'],
-                    'cabin_name' => $category_data['name'],
+                    'cabin_name' => $category_name,
+                    'cabin_id' => $cabin_category_id,
                     'price_places' => $places,
-                    'price_value' => intval($cost['std_3'] ?? 0),
-                    'eds' => true
+                    'price_value' => $price_value,
+                    'eds' => true,
                 ]);
 
                 $rooms[] = [
                     'n' => $gama_cabin_num,
-                    'd' => $record['deck_id']
+                    'd' => $record['deck_id'],
                 ];
             }
         }
