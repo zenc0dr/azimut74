3e96ed3e9e5f49b98fd71ef624df4c7352335b54
Al Blaze <zen@8ber.ru>
2026-06-08 18:51:15 +0400

    https://azimuth-tour.kaiten.ru/65841713

--- themes/azimut-tur-pro/src/components/header/TopMenu.vue
+++ themes/azimut-tur-pro/src/components/header/TopMenu.vue
@@ -1,19 +1,19 @@
 <template>
     <div v-click-outside="closeDropdowns" class="top-menu" v-if="width !== null">
         <MenuTree
-            v-if="width > 993"
+            v-if="show_desktop_menu"
             :items="items"
             :root-classes="root_classes"
             :item-classes="item_classes"
             :carets="carets"
             :click-outside="click_outside"
         />
-        <div class="top-menu_mobile bg-blue-300" v-if="width <= 992">
+        <div class="top-menu_mobile bg-blue-300" v-if="show_mobile_menu">
             <div class="container">
                 <div class="py-2 d-flex">
                     <div class="col-auto top-menu_mobile-humburger">
                         <i class="bi bi-list fs-h1 text-white"
-                           @click="clickHaburger()"
+                           @click.stop="clickHaburger()"
                            style="cursor:pointer">
                         </i>
                     </div>
@@ -40,7 +40,7 @@
 
                     <div class="col-auto col-auto d-flex align-items-center justify-content-center">
                         <!--<div class="whats-app-btn kruiz_injector3 only-river-crs" onclick="window.open('https://wa.me/79271149850?text=Меня интересуют речные круизы')"></div>-->
-                        <i class="bi bi-telephone-inbound fs-h1 text-white" @click="clickContacts()" style="cursor:pointer"></i>
+                        <i class="bi bi-telephone-inbound fs-h1 text-white" @click.stop="clickContacts()" style="cursor:pointer"></i>
                     </div>
 
                 </div>
@@ -56,24 +56,16 @@
             </div>
 
             <div v-if="contacts_menu" class="top-menu__contacts-menu bg-primary-100 py-4">
-                <div class="container">
-                    <div v-if="!is_rivercrs" class="top-menu__contacts-menu__phones col-12 mb-3  d-flex flex-column align-items-center">
-                        <div class="top-menu__contacts-menu__phone">
-                            <a href="tel:88452990819" class="text-decoration-none fs-h2 c-primary-200">
-                                8 (8452) 99 08 19<i style="margin-left: 15px" class="bi bi-telephone-forward"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="d-flex flex-column text-center">
-                        <div style="display:none" class="fs-ss mb-1">Круизный отдел</div>
-                        <a style="display:none" href="tel:88452990819" class="text-decoration-none c-primary-200 fs-def fw-bolder">
-                            8 (8452) 99 08 19
-                        </a>
-                        <a href="tel:88001004959" class="text-decoration-none c-primary-200 fs-def fw-bolder">
-                            8 (800) 100 49 59
-                        </a>
-                        <div class="fs-ss mb-1">Звонок бесплатный</div>
-                    </div>
+                <div class="container d-flex flex-column align-items-center text-center">
+                    <a v-if="!is_rivercrs"
+                       href="tel:88452990819"
+                       class="text-decoration-none fs-h2 c-primary-200 mb-2">
+                        8 (8452) 99 08 19<i style="margin-left: 15px" class="bi bi-telephone-forward"></i>
+                    </a>
+                    <a href="tel:88001004959" class="text-decoration-none c-primary-200 fs-def fw-bolder">
+                        8 (800) 100 49 59
+                    </a>
+                    <div class="fs-ss mt-1">Звонок бесплатный</div>
                 </div>
             </div>
         </div>
@@ -83,6 +75,9 @@
 import MenuTree from "./MenuTree";
 import SliderMenu from "./SliderMenu";
 import ClickOutside from "./directives/ClickOutside";
+
+const MOBILE_BREAKPOINT = 992;
+const CONTACTS_CLICK_GUARD_MS = 350;
 
 export default {
     name: 'TopMenu',
@@ -100,7 +95,7 @@ export default {
             is_rivercrs: false,
             mobile_menu: false,
             contacts_menu: false,
-            //mgo_phone: null,
+            last_contacts_click: 0,
             width: typeof window !== 'undefined' ? window.innerWidth : null,
             root_classes: [
                 'd-flex justify-content-left',
@@ -118,19 +113,22 @@ export default {
             click_outside: false
         }
     },
-    watch: {
-        /*
-        contacts_menu(active) {
-            if (active) {
-                this.mgo_phone = $('.mgo-number-29808').first().text()
-            }
+    computed: {
+        show_mobile_menu() {
+            return this.width <= MOBILE_BREAKPOINT || this.contacts_menu || this.mobile_menu
         },
-         */
+        show_desktop_menu() {
+            return this.width > MOBILE_BREAKPOINT && !this.contacts_menu && !this.mobile_menu
+        }
+    },
+    watch: {
         width(width) {
-            if (width > 992) {
+            if (width > MOBILE_BREAKPOINT) {
                 $('body').css('padding-top', 0)
             } else {
-                this.mobile_menu = false
+                if (!this.contacts_menu) {
+                    this.mobile_menu = false
+                }
                 $('body').css('padding-top', '57px')
             }
         }
@@ -142,13 +140,16 @@ export default {
             this.switchShadow()
         },
         clickContacts() {
+            const now = Date.now()
+            if (now - this.last_contacts_click < CONTACTS_CLICK_GUARD_MS) {
+                return
+            }
+            this.last_contacts_click = now
             this.contacts_menu = !this.contacts_menu
             this.mobile_menu = false
             this.switchShadow()
         },
         switchShadow() {
-
-
             if (!this.mobile_menu && !this.contacts_menu) {
                 if ($('body').hasClass('shadow')) $('body').toggleClass('shadow')
             } else {
@@ -165,12 +166,15 @@ export default {
             })
         },
         setWidth() {
-            //this.width = Math.max(window.innerWidth, document.documentElement.clientWidth)
-            this.width = $('body').width()
-
+            this.width = window.innerWidth
         },
         closeDropdowns() {
             this.click_outside = !this.click_outside
+            if (this.contacts_menu || this.mobile_menu) {
+                this.contacts_menu = false
+                this.mobile_menu = false
+                this.switchShadow()
+            }
         }
     }
 }
